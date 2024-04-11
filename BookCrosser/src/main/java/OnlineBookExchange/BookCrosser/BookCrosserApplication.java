b5364c331930467eab145909a3b7096a0cc90fc7
Krishnakumar <89072286+Krishnaselvam@users.noreply.github.com>
2024-04-11 13:16:43 +0300

    Just set up a project

---
+++ BookCrosser/src/main/java/OnlineBookExchange/BookCrosser/BookCrosserApplication.java
@@ -0,0 +1,13 @@
+package OnlineBookExchange.BookCrosser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookCrosserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookCrosserApplication.class, args);
+	}
+
+}
